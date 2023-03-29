4a9777b9eb3e0f4a938c35960f9eb0918a87df24
alvinle29 <bach938@gmail.com>
2023-03-29 17:22:47 +0700

    implement models Model and get the data from OpenAPI

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
+import 'package:chat_gpt_application/models/models_model.dart';
 import 'package:http/http.dart' as http;
 import '../constants/api_constants.dart';
 
 class ApiService {
-  static Future<void> getModels() async {
+  static Future<List<ModelsModel>> getModels() async {
     try {
       var response = await http.get(
         Uri.parse("$BASE_URL/models"),
@@ -18,9 +20,17 @@ class ApiService {
         print("jsonResponse['error'] ${jsonResponse['error']["message"]}");
         throw HttpException(jsonResponse['error']["message"]);
       }
-      print("jsonResponse $jsonResponse");
+      //print("jsonResponse $jsonResponse");
+
+      List temp = [];
+      for (var value in jsonResponse["data"]) {
+        temp.add(value);
+        log("temp ${value["id"]}");
+      }
+      return ModelsModel.modelsFromSnapshot(temp);
     } catch (error) {
       print("error $error");
+      rethrow;
     }
   }
 }
