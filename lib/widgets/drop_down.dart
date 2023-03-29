ec9a765e0ee958d4fb234fb0be3ed135a565df47
alvinle29 <bach938@gmail.com>
2023-03-29 17:49:45 +0700

    display model list on the dropdown using future builder

--- lib/widgets/drop_down.dart
+++ lib/widgets/drop_down.dart
@@ -1,4 +1,7 @@
 import 'package:chat_gpt_application/constants/constants.dart';
+import 'package:chat_gpt_application/models/models_model.dart';
+import 'package:chat_gpt_application/services/api_services.dart';
+import 'package:chat_gpt_application/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
 class ModelDropDownWidget extends StatefulWidget {
@@ -9,19 +12,40 @@ class ModelDropDownWidget extends StatefulWidget {
 }
 
 class _ModelDropDownWidgetState extends State<ModelDropDownWidget> {
-  String currentModel = "Model1";
+  String currentModel = "text-davinci-003";
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-        dropdownColor: scaffoldBackgroundColor,
-        iconEnabledColor: Colors.white,
-        items: getModelsItem,
-        value: currentModel,
-        onChanged: (value) {
-          setState(() {
-            currentModel = value.toString();
-          });
-        });
+    return FutureBuilder<List<ModelsModel>>(
+      future: ApiService.getModels(),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: TextWidget(label: snapshot.error.toString()),
+          );
+        }
+        return snapshot.data == null || snapshot.data!.isEmpty
+            ? const SizedBox.shrink()
+            : FittedBox(
+              child: DropdownButton<String>(
+                  dropdownColor: scaffoldBackgroundColor,
+                  iconEnabledColor: Colors.white,
+                  items: List<DropdownMenuItem<String>>.generate(
+                      snapshot.data!.length,
+                      (index) => DropdownMenuItem(
+                          value: snapshot.data![index].id,
+                          child: TextWidget(
+                            label: snapshot.data![index].id,
+                            fontSize: 15,
+                          ))),
+                  value: currentModel,
+                  onChanged: (value) {
+                    setState(() {
+                      currentModel = value.toString();
+                    });
+                  }),
+            );
+      },
+    );
   }
 }
